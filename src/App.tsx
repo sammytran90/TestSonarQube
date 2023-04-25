d9cd5b672aeddf3184cd4184a5562451bdb7106d
TrungTran <trung.trananh0907@gmail.com>
2023-04-25 14:27:09 +0100

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <HomePage></HomePage>
     </div>
   );
